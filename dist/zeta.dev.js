544f1ac4aa1a24a12ad8c4514e09ffdee9931784
szpatis21 <124190923+szpatis21@users.noreply.github.com>
2023-04-28 10:56:11 +0200

    Add files via upload

---
+++ dist/zeta.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function check() {
+  elfogadom = reg2.confirm;
+
+  if (document.getElementById("confirmi").checked == false) {
+    alert('El kell fogadnia az adatkezelési tájékoztatóban foglaltakat, hogy tovább léphessen!');
+  }
+}
